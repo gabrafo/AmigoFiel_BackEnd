2f37791228a103beead42e67a1cc484c1699f51f
douglas <douglasholanda3195@gmail.com>
2024-07-30 09:00:32 -0400

    CRUD basic methods in Adoptant Service

--- src/main/java/br/com/amigofiel/services/AdoptantService.java
+++ src/main/java/br/com/amigofiel/services/AdoptantService.java
@@ -7,6 +7,9 @@ import br.com.amigofiel.repositories.AdoptantRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @AllArgsConstructor
 @Service
 public class AdoptantService {
@@ -19,4 +22,19 @@ public class AdoptantService {
         return adoptantRepository.save(newAdoptant);
     }
 
+    public Optional<Adoptant> findAdoptantById(AdoptantDTO adoptantDTO) {
+        Adoptant newAdoptant = adoptantMapper.toEntity(adoptantDTO);
+        return adoptantRepository.findById(newAdoptant.getId());
+    }
+    public List<Adoptant> listAllAdoptants() {
+        return adoptantRepository.findAll();
+    }
+    /* TODO()
+    public Adoptant updateAdoptant(Adoptant adoptant){
+    } */
+
+    public void deleteAdoptantById(AdoptantDTO adoptantDTO) {
+        Adoptant newAdoptant = adoptantMapper.toEntity(adoptantDTO);
+        adoptantRepository.deleteById(newAdoptant.getId());
+    }
 }
