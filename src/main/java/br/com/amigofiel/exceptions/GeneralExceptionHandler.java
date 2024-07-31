e396ea6b7291e9afa05cefee387e0d85b25e1c6c
gabrafo <gafagundesmesquita@gmail.com>
2024-07-31 11:44:10 -0300

    basic exception handling for NotFoundException

--- src/main/java/br/com/amigofiel/exceptions/GeneralExceptionHandler.java
+++ src/main/java/br/com/amigofiel/exceptions/GeneralExceptionHandler.java
@@ -1,7 +1,16 @@
 package br.com.amigofiel.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GeneralExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    private ResponseEntity<RestErrorMessage> entityNotFoundException(NotFoundException e) {
+        RestErrorMessage errorMessage = new RestErrorMessage(HttpStatus.NOT_FOUND, e.getMessage());
+        return ResponseEntity.status(errorMessage.status()).body(errorMessage);
+    }
 }
