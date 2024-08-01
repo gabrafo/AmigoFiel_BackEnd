ba0642d5cb9f3190e0155b671f948f2fb0ddfd8d
douglas <douglasholanda3195@gmail.com>
2024-07-31 20:55:40 -0400

    Controller of Adoptant

---
+++ src/main/java/br/com/amigofiel/controller/AnimalController.java
@@ -0,0 +1,9 @@
+package br.com.amigofiel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/animals")
+public class AnimalController {
+}
