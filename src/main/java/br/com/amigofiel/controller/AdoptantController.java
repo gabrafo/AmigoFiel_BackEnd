db4ce92e331ae223a28a52b02796a80b5db5ad01
douglas <douglasholanda3195@gmail.com>
2024-08-02 22:18:25 -0400

    New configuration in Adoptant controller

--- src/main/java/br/com/amigofiel/controller/AdoptantController.java
+++ src/main/java/br/com/amigofiel/controller/AdoptantController.java
@@ -3,7 +3,12 @@ package br.com.amigofiel.controller;
 import br.com.amigofiel.domain.dto.AdoptantDTO;
 import br.com.amigofiel.domain.entities.Adoptant;
 import br.com.amigofiel.services.AdoptantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,18 +19,52 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/adoptants")
-@Tag(name = "Adopatant", description = "Endpoints relacionados a Adoptant")
+@Tag(name = "Adoptant", description = "Endpoints relacionados a Adoptant")
 public class AdoptantController {
 
     @Autowired
     private AdoptantService adoptantService;
 
 
-    @PostMapping("/adoptants")
-    public ResponseEntity<String> saveAdoptant(@RequestBody AdoptantDTO adoptantDTO) {
-        adoptantService.createAdoptant(adoptantDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Adoptant " + adoptantDTO.name() + " created successfully!");
+    @Operation(summary = "Cria um adotante",
+            description = "Cria um novo adotante no banco de dados.",
+            tags = {"Adoptant"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = AdoptantDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
+    @PostMapping(value = "/create",
+            produces = "application/json",
+            consumes = "application/json")
+    public ResponseEntity<Adoptant> saveAdoptant(@Valid @RequestBody AdoptantDTO adoptantDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(adoptantService.createAdoptant(adoptantDTO));
     }
+
+    @Operation(summary = "Lista os adotantes",
+            description = "Lista todos os adotantes do Banco de Dados.",
+            tags = {"Adotante"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = AdoptantDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
+    @GetMapping(value = "list_adoptants",
+            produces = "application/json")
+    public ResponseEntity<List<Adoptant>> listAdoptants() {
+        return ResponseEntity.status(HttpStatus.OK).body(adoptantService.findAllAdoptants())
+    }
+
+
+
 
     @GetMapping("/{id}")
     public ResponseEntity<Adoptant> findAdoptantById(@PathVariable("id") Long id) {
@@ -44,24 +83,6 @@ public class AdoptantController {
             return ResponseEntity.ok(adoptant);
         } else {
             return ResponseEntity.notFound().build();
-        }
-    }
-
-    @GetMapping("list_adoptants")
-    public ResponseEntity<List<AdoptantDTO>> listAdoptants() {
-        List<Adoptant> adoptants = adoptantService.findAllAdoptants();
-        List<AdoptantDTO> adoptantDTOs = new ArrayList<>();
-
-        if (!adoptants.isEmpty()) {
-            for (Adoptant adoptant : adoptants) {
-                adoptantDTOs.add(new AdoptantDTO(adoptant.getId(), adoptant.getName(),
-                        adoptant.getBirthDate(), adoptant.getGender(),
-                        adoptant.getAddress(), adoptant.getAdoptions(),
-                        adoptant.getUser()));
-            }
-            return ResponseEntity.ok(adoptantDTOs);
-        } else {
-            return ResponseEntity.noContent().build();
         }
     }
 
